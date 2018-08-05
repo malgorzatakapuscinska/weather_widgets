b5552608aecb310146d7a774b36720ad3d7a8bf6
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-08-05 17:00:39 +0200

    Configure webpack part 1 - babel, react

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './src/index.js',
+    output : {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.bundle.js'
+  }
+  module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
